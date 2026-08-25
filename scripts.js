bfb0e19cbf7129bc09d82d6b75b7fee3a4d77cf0
adrianacostx <adrianacostaperozo@gmail.com>
2026-08-25 12:06:33 -0400

    some changes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const UNLOCK_NAME = ['lorena', 'liliana', 'estefania', 'lili']; // expected input, lowercase
+const UNLOCK_NAME = ['mercy','gabriela','gabi', 'gabito']; // expected input, lowercase
 
 // --- Matrix rain background ---
 (function () {
